858de49822ecfcb98e2bf98059618099a49424fb
daniel.cahall <danielenricocahall@gmail.com>
2025-12-02 22:29:19 -0500

    part 2 done

--- day_2/solution.py
+++ day_2/solution.py
@@ -1,20 +1,36 @@
 
 
-def count_invalid_ids(id_range: str) -> int:
+def count_invalid_ids(id_range: str, max_repeats: int | None = None) -> int:
+
+    def are_there_repeats(_id):
+
+        L = len(_id)
+        for block_size in range(1, L // 2 + 1):
+            if L % block_size != 0:
+                continue
+            repeats = L // block_size
+            if repeats < 2:
+                continue
+            if max_repeats and repeats != max_repeats:
+                continue
+            block = _id[:block_size]
+            if block * repeats == _id:
+                return True
+        return False
+
     start, end = map(int, id_range.split("-"))
     sum_ = 0
     for _id in range(start, end + 1):
         _id = str(_id)
-        left, right = _id[:len(_id) // 2], _id[len(_id) // 2:]
-        if left == right:
+        if are_there_repeats(_id):
             sum_ += int(_id)
     return sum_
+
 
 if __name__ == "__main__":
     with open('./puzzle.txt') as fp:
         id_ranges = fp.readline().split(",")
     sum_of_invalid_ids = 0
     for id_range in id_ranges:
-        print(id_range)
-        sum_of_invalid_ids += count_invalid_ids(id_range)
+        sum_of_invalid_ids += count_invalid_ids(id_range, None)
     print(sum_of_invalid_ids)
