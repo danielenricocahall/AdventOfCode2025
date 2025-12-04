042548f5f4f604619e53cafeb7802363cf20a680
daniel.cahall <danielenricocahall@gmail.com>
2025-12-03 21:41:37 -0500

    add friction logic

--- day_3/solution.py
+++ day_3/solution.py
@@ -1,12 +1,13 @@
 from itertools import combinations
 
 
-def find_max_joltage(bank: list[int]):
+def find_max_joltage(bank: list[int], friction: bool = False):
     # brute force
-    battery_combos = set(combinations(bank, r=2))
+    r = 2 if not friction else 12
+    battery_combos = set(combinations(bank, r=r))
     max_joltage = 0
-    for first_battery, second_battery in battery_combos:
-        joltage = int(f"{first_battery}{second_battery}")
+    for batteries in battery_combos:
+        joltage = int("".join(map(str, batteries)))
         max_joltage = max(joltage, max_joltage)
     return max_joltage
 
@@ -14,8 +15,8 @@ def find_max_joltage(bank: list[int]):
 
 
 if __name__ == "__main__":
-    with open("./puzzle.txt") as fp:
+    with open("./puzzle_test.txt") as fp:
         banks = list(map(lambda x: list(str.strip(x)), fp.readlines()))
         banks = [list(map(int, x)) for x in banks]
-        max_joltages = [find_max_joltage(bank) for bank in banks]
+        max_joltages = [find_max_joltage(bank, friction=True) for bank in banks]
         print(sum(max_joltages))
